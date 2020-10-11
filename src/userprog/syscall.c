0421d03bed45d1a02cb373d6000c96044519057a
Norgannon <2300776402@qq.com>
2020-10-12 00:24:26 +0800

    write

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,10 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "userprog/process.h"
+#include "filesys/file.h"
+#include "filesys/filesys.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -17,4 +21,64 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
+}
+void syscall_halt (void){
+  shutdown_power_off();
+}
+void syscall_exit (int status){
+
+}
+pid_t syscall_exec (const char *file){
+
+}
+int syscall_wait (pid_t pid){
+
+}
+bool syscall_create (const char *file, unsigned initial_size){
+
+}
+bool syscall_remove (const char *file){
+
+}
+int syscall_open (const char *file){
+
+}
+int syscall_filesize (int fd){
+
+}
+int syscall_read (int fd, void *buffer, unsigned length){
+
+}
+int syscall_write (int fd, const void *buffer, unsigned length){
+  if(fd==STDOUT){
+    putbuf(buffer,length);
+    return length;
+  }else{
+    struct file *f = get_file_by_fd(fd);
+    if(f==NULL){
+      exit(-1);
+    }
+    return (int) file_write(f,buffer,length);
+
+  }
+}
+void syscall_seek (int fd, unsigned position){
+
+}
+unsigned syscall_tell (int fd){
+
+}
+void syscall_close (int fd){
+
+}
+
+struct file*
+get_file_by_fd(int fd){
+  struct thread *current_thread=thread_current ();
+  struct list_elem *tmp;
+  for (tmp = list_begin (&current_thread->file_list); tmp != list_end (&current_thread->file_list); tmp = list_next (tmp)){
+    if(list_entry (tmp, struct process_file, elem)->fd== fd)
+      return list_entry (tmp, struct process_file, elem)->file;
+  }
+  return NULL;
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,48 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include <stdbool.h>
+#include <debug.h>
+/* Process identifier. */
+typedef int pid_t;
+#define PID_ERROR ((pid_t) -1)
+
+/* Map region identifier. */
+typedef int mapid_t;
+#define MAP_FAILED ((mapid_t) -1)
+
+/* Maximum characters in a filename written by readdir(). */
+#define READDIR_MAX_LEN 14
+
+/* Typical return values from main() and arguments to exit(). */
+#define EXIT_SUCCESS 0          /* Successful execution. */
+#define EXIT_FAILURE 1          /* Unsuccessful execution. */
+
+#define STDIN 0
+#define STDOUT 1
+
+struct process_file{
+    int fd;
+    struct file *file;
+    struct list_elem elem;
+}
+
 void syscall_init (void);
+
+void syscall_halt (void);
+void syscall_exit (int status);
+pid_t syscall_exec (const char *file);
+int syscall_wait (pid_t pid);
+bool syscall_create (const char *file, unsigned initial_size);
+bool syscall_remove (const char *file);
+int syscall_open (const char *file);
+int syscall_filesize (int fd);
+int syscall_read (int fd, void *buffer, unsigned length);
+int syscall_write (int fd, const void *buffer, unsigned length);
+void syscall_seek (int fd, unsigned position);
+unsigned syscall_tell (int fd);
+void syscall_close (int fd);
+
+struct file* get_file_by_fd(int fd);
 
 #endif /* userprog/syscall.h */
