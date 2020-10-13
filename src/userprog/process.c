3b4f568f38c79eaffa14caacd396797e91ba3fbc
Norgannon-thedreamweaver <vigilant1028@gmail.com>
2020-10-13 19:13:57 +0800

    deny write

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -68,7 +68,7 @@ start_process (void *file_name_)
   /* If load failed, quit. */
   palloc_free_page (file_name);
   if (!success) 
-    thread_exit ();
+    33322-2=
 
   /* Start the user process by simulating a return from an
      interrupt, implemented by intr_exit (in
